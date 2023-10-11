1dd6eb93e6bee72a70d9a8a464353bf8ff1b3b36
Monzerali1 <monzercojoker1@gmail.com>
2023-10-11 10:40:42 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * description: print _putchar using putchar prototype
+ *
+ * return: always 0 (succss)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
