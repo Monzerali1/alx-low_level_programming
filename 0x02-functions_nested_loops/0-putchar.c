904c2072f39b50403a21a62b66bedfbb0cf4cddf
Monzerali1 <monzercojoker1@gmail.com>
2023-10-11 11:11:48 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
-
+	/* tis is the main function */
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
-
+	/* it returns 0 to indicate successful execution */
 	return (0);
 }
