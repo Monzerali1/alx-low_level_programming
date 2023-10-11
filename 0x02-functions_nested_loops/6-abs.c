7696eaeb8b18723b8fea5723e873677fba8619db
Monzerali1 <monzercojoker1@gmail.com>
2023-10-11 15:08:52 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolut value of an integer
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: always 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (c < 0)
+		c = (-1) * c;
+	return (c);
+}
