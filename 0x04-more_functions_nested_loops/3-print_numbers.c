917d9af6c1c452e9faad3f5c7cb189928dbaf88a
Monzerali1 <monzercojoker1@gmail.com>
2023-10-12 15:00:50 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,11 @@
 
 void print_numbers(void);
 {
-	char c;
+	int c = 0;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
+	do {
+		_putchar(c + 48);
+		c++;
+	} while (c >= 0 && c <= 9);
 	_putchar('\n');
 }
