d9f643b504bafd43c3411fb34516132b2053d26b
Monzerali1 <monzercojoker1@gmail.com>
2023-10-12 14:42:21 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - print 0 - 9
+ *		onle using _putchar twice
  *
  * Return: always 0
 */
@@ -9,7 +10,7 @@
 void print_numbers(void);
 {
 	int num = 0;
-	
+
 	do {
 		_putchar(num + 48);
 		num++;
