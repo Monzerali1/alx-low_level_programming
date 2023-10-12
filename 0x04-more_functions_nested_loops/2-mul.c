b3cd011828b0d26269b5ae8e3332e74f950f63f7
Monzerali1 <monzercojoker1@gmail.com>
2023-10-12 10:25:54 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * mul - multiplies to integrs
+ *
+ * @c: fiest input
+ * @c: second input
+ *
+ * Return: return results
+*/
+
+int mul(int c, int c)
+{
+	return (c * c);
+}
