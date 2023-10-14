254959ce02e45703807f726c80532bfe3f782ae9
Monzerali1 <monzercojoker1@gmail.com>
2023-10-14 06:11:24 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,10 +31,10 @@ void print_times_table(int n)
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
 				} else if (prod <= 99 && prod >= 10)
-					_putcahr((prod / 10) + 48);
+					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
-			__putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
