67d1eb87b6255fa65362fbee19313f0140edac65
Monzerali1 <monzercojoker1@gmail.com>
2023-10-19 12:44:36 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,7 +25,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		l = j;
 	if (l + 1 > size_r)
 		return (0);
-	r[l] '\0';
+	r[l] = '\0';
 	for (k = l - 1; k >= 0; k--)
 	{
 		i--;
@@ -46,9 +46,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[l + 1] = '\0';
 		if (l + 2 > size_r)
 			return (0);
-		while ((l-- >= 0)
-				r[l + 1] = r[l];
-				r[0] = d + '0';
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
 	return (r);
 }
