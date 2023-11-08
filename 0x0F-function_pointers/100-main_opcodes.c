9ea45c22e5f0713f7705556c6c645fc3fc9f82e0
Monzeral1 <monzercojoker1@gmail.com>
2023-11-08 11:31:50 -0800

    a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,6 @@
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
 
 /**
  * main - check the code for the school stuadents
@@ -15,10 +15,10 @@ int main(int argc, char **argv)
 	int b;
 
 	if (argc != 2)
-		printf("error\n"), exit(1);
+		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
 	if (b < 0)
-		printf("error\n"), exit(2);
+		printf("Error\n"), exit(2);
 	while (b--)
 		printf("%02hhx%s", *p++, b ? " " : "\n");
 	return (0);
