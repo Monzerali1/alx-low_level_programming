1b3b3416144998abbc39e4959043311071c39a45
Monzeral1 <monzercojoker1@gmail.com>
2023-11-08 11:08:09 -0800

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds tow integrs
+ * @a: the first integr
+ * @b: the second integr
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts tow integrs
+ * @a: the first integr
+ * @b: the second integr
+ * Return: the difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies tow integrs
+ * @a: the first integr
+ * @b: the second integr
+ * Return: the product
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides tow integrs
+ * @a: the first integr
+ * @b: the second integr
+ * Return: the qoutient
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo tow integrs
+ * @a: the first integr
+ * @b: the second integr
+ * Return: the remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
